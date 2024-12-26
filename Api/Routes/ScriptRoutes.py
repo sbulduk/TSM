628f13d28201c97da8f0173c441525640dc90580
Serdar Bulduk <sbulduk@taskin-logistics.com>
2024-12-26 08:07:10 +0100

    Script routes updated.
    New endpoints were added for the ScriptService.

--- Api/Routes/ScriptRoutes.py
+++ Api/Routes/ScriptRoutes.py
@@ -14,8 +14,30 @@ def ExecuteScript():
         print("Exit Code:",result["status_code"])
         return jsonify({"success":True,"data":f"{result}"}),200
     except Exception as e:
-        return({"success":False,"data":f"Check user credentials."})
+        return({"success":False,"data":f"Check user credentials."}),400
     
 @ScriptBlueprint.route("/invokeclassmethod",methods=["POST"])
 def InvokeClassMethod():
+    try:
+        # TODO: Turn this static parameters into dynamic, to be read from json file!!!
+        script=scriptService.InvokeClassMethod(
+            className="ComputerHelper",
+            methodName="CheckComputerExists",
+            params={"computerName":"AS-TASKIN162"}
+        )
+        return jsonify({"success":True,"data":f"{script}"}),200
+    except Exception as e:
+        return jsonify({"success":False,"data":f"InvokeClassMethod Error: {e}"}),400
     
+@ScriptBlueprint.route("/runscriptfile",methods=["POST"])
+def RunScriptFile():
+    try:
+        filePath="..\\Services\\ScriptService\\Scripts\\ComputerHelper.ps1"
+        params={"OUPath":"OU=Computers,DC=blahblah,DC=comblah"}
+        script=scriptService.RunScriptFile(
+            filePath=filePath,
+            params=params
+        )
+        return jsonify({"success":True,"data":f"{script}"}),200
+    except Exception as e:
+        return jsonify({"success":False,"data":f"RunScriptFile Error: {e}"}),400
